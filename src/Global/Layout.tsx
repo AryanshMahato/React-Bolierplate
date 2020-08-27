af2a1054a911b3e566e9b0524a431aae925ece7a
Aryansh Mahato <aryanshmahato@gmail.com>
2020-08-27 11:22:26 +0530

    Landing Page Completed

--- src/Global/Layout.tsx
+++ src/Global/Layout.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import ThemeProvider from './ThemeProvider';
 
 interface Props {
   children: React.ReactNode;
 }
 
 const Layout: React.FC<Props> = ({ children }: Props) => {
-  return <>{children}</>;
+  return <ThemeProvider>{children}</ThemeProvider>;
 };
 
 export default Layout;
--- src/Pages/Landing.tsx
+++ src/Pages/Landing.tsx
@@ -1,7 +1,55 @@
 import React from 'react';
+import { Box, Button, Typography } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import { useHistory } from 'react-router';
 
 const Landing: React.FC = () => {
-  return <>Landing Page</>;
+  const classes = useStyles();
+  const { push } = useHistory();
+
+  const signUpClicked = () => {
+    push('/signup');
+  };
+  const loginClicked = () => {
+    push('/login');
+  };
+
+  return (
+    <Box
+      display={'flex'}
+      height={'80vh'}
+      justifyContent={'center'}
+      alignItems={'center'}
+      flexDirection={'column'}
+      textAlign={'center'}
+    >
+      <Typography variant={'h2'} className={classes.annotation}>
+        Welcome to the Ecommerce Website
+      </Typography>
+      <Box className={classes.buttons}>
+        <Button onClick={signUpClicked} variant={'contained'} color={'primary'}>
+          Sign Up
+        </Button>
+        <Button onClick={loginClicked} variant={'contained'} color={'primary'}>
+          Login
+        </Button>
+      </Box>
+    </Box>
+  );
 };
+
+const useStyles = makeStyles((theme) => ({
+  annotation: {
+    fontWeight: 'bold',
+  },
+  buttons: {
+    width: '80vw',
+    maxWidth: '250px',
+    display: 'flex',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    marginTop: theme.spacing(5),
+  },
+}));
 
 export default Landing;
